7ab50e429c5fba20d78618108634b5db02317106
williamchilito <168607387+williamchilito@users.noreply.github.com>
2024-05-12 18:53:50 -0500

    Update index.blade.php

--- Pagina/resources/views/index.blade.php
+++ Pagina/resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.include')
+io Electrónico@extends('layouts.include')
 @section('content')
 
 <!-- Start Banner Hero -->
@@ -18,11 +18,11 @@
                     <div class="col-lg-6 mb-0 d-flex align-items-center">
                         <div class="text-align-left align-self-center">
                             <h1 class="h1 text-success"><b>Zay</b> eCommerce</h1>
-                            <h3 class="h2">Tiny and Perfect eCommerce Template</h3>
+                            <h3 class="h2">Comercio Electrónico</h3>
                             <p>
-                                Zay Shop is an eCommerce HTML5 CSS template with latest version of Bootstrap 5 (beta 1). 
-                                This template is 100% free provided by <a rel="sponsored" class="text-success" href="https://templatemo.com" target="_blank">TemplateMo</a> website. 
-                                Image credits go to <a rel="sponsored" class="text-success" href="https://stories.freepik.com/" target="_blank">Freepik Stories</a>,
+                                Tienda deportiva, ofrecemos los mejores elementos deportivos, calidad, precio insuperable. 
+                                Plantilla proporcionada por <a rel="sponsored" class="text-success" href="https://templatemo.com" target="_blank">TemplateMo</a> website. 
+                                Imagenes creditos a <a rel="sponsored" class="text-success" href="https://stories.freepik.com/" target="_blank">Freepik Stories</a>,
                                 <a rel="sponsored" class="text-success" href="https://unsplash.com/" target="_blank">Unsplash</a> and
                                 <a rel="sponsored" class="text-success" href="https://icons8.com/" target="_blank">Icons 8</a>.
                             </p>
@@ -84,28 +84,27 @@
 <section class="container py-5">
     <div class="row text-center pt-3">
         <div class="col-lg-6 m-auto">
-            <h1 class="h1">Categories of The Month</h1>
+            <h1 class="h1">Categorias del Mes</h1>
             <p>
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                Los productos mas vendidos y buscados del mes.
             </p>
         </div>
     </div>
     <div class="row">
         <div class="col-12 col-md-4 p-5 mt-3">
             <a href="#"><img src="./assets/img/category_img_01.jpg" class="rounded-circle img-fluid border"></a>
-            <h5 class="text-center mt-3 mb-3">Watches</h5>
-            <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
+            <h5 class="text-center mt-3 mb-3">Relojes</h5>
+            <p class="text-center"><a class="btn btn-success">Ir a la tienda</a></p>
         </div>
         <div class="col-12 col-md-4 p-5 mt-3">
             <a href="#"><img src="./assets/img/category_img_02.jpg" class="rounded-circle img-fluid border"></a>
-            <h2 class="h5 text-center mt-3 mb-3">Shoes</h2>
-            <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
+            <h2 class="h5 text-center mt-3 mb-3">Zapatos</h2>
+            <p class="text-center"><a class="btn btn-success">Ir a la tienda</a></p>
         </div>
         <div class="col-12 col-md-4 p-5 mt-3">
             <a href="#"><img src="./assets/img/category_img_03.jpg" class="rounded-circle img-fluid border"></a>
-            <h2 class="h5 text-center mt-3 mb-3">Accessories</h2>
-            <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
+            <h2 class="h5 text-center mt-3 mb-3">Accesorios</h2>
+            <p class="text-center"><a class="btn btn-success">Ir a la tienda</a></p>
         </div>
     </div>
 </section>
@@ -117,10 +116,9 @@
     <div class="container py-5">
         <div class="row text-center py-3">
             <div class="col-lg-6 m-auto">
-                <h1 class="h1">Featured Product</h1>
+                <h1 class="h1">Producto destacado</h1>
                 <p>
-                    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident.
+                    El producto estrella.
                 </p>
             </div>
         </div>
@@ -141,11 +139,11 @@
                             </li>
                             <li class="text-muted text-right">$240.00</li>
                         </ul>
-                        <a href="/detalles" class="h2 text-decoration-none text-dark">Gym Weight</a>
+                        <a href="/detalles" class="h2 text-decoration-none text-dark">Gimnasio</a>
                         <p class="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.
+                            Para mantenerte en forma.
                         </p>
-                        <p class="text-muted">Reviews (24)</p>
+                        <p class="text-muted">Vistas (24)</p>
                     </div>
                 </div>
             </div>
@@ -165,11 +163,11 @@
                             </li>
                             <li class="text-muted text-right">$480.00</li>
                         </ul>
-                        <a href="/detalles" class="h2 text-decoration-none text-dark">Cloud Nike Shoes</a>
+                        <a href="/detalles" class="h2 text-decoration-none text-dark">Zapatillas Nike</a>
                         <p class="card-text">
-                            Aenean gravida dignissim finibus. Nullam ipsum diam, posuere vitae pharetra sed, commodo ullamcorper.
+                            Excelentes.
                         </p>
-                        <p class="text-muted">Reviews (48)</p>
+                        <p class="text-muted">Vistas (48)</p>
                     </div>
                 </div>
             </div>
@@ -189,11 +187,11 @@
                             </li>
                             <li class="text-muted text-right">$360.00</li>
                         </ul>
-                        <a href="/detalles" class="h2 text-decoration-none text-dark">Summer Addides Shoes</a>
+                        <a href="/detalles" class="h2 text-decoration-none text-dark">Zapatillas Adidas</a>
                         <p class="card-text">
-                            Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec.
+                            Serás la envidia de todos.
                         </p>
-                        <p class="text-muted">Reviews (74)</p>
+                        <p class="text-muted">Vistas (74)</p>
                     </div>
                 </div>
             </div>
